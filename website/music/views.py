c6f33f29b54cc3c2a80bd2f9826b920f06ef7fea
Aayush <aayushdhakal360@gmail.com>
2018-09-08 18:54:43 +0545

    adding navbar and background image

--- website/music/views.py
+++ website/music/views.py
@@ -23,7 +23,10 @@ def favourite(request, album_id):
 		return render(request, 'music/details.html', {'albums':album,
 		'error_message': "You selected an invalid song"} )
 	else:
-		selected_song.is_favourite=True
+		if selected_song.is_favourite==False:
+			selected_song.is_favourite=True
+		else:
+			selected_song.is_favourite=False
 		selected_song.save()
 		return render(request, 'music/details.html', {'albums':album})
 		
